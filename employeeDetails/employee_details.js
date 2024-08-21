022843769582a496bc4f7bf7bbc91dac28459886
ibeepboop <michellemeyer660@gmail.com>
2024-08-21 16:35:34 -0400

    added Find Employee By Specialization

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'Javascript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Python' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Java' },
 ]
 function displayEmployees() {
     const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
@@ -26,5 +26,14 @@ function findEmployeeById(employeeId) {
         document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
     } else {
         document.getElementById('employeesDetails').innerHTML = 'No employee has been found with this ID';
+    }
+}
+
+function findEmployeeBySpecialization(empSpecialization) {
+    const findEmployeeBySpecialization = employees.find(employee => employee.specialization === empSpecialization);
+    if (findEmployeeBySpecialization) {
+        document.getElementById('employeesDetails').innerHTML = `<p>${findEmployeeBySpecialization.id}: ${findEmployeeBySpecialization.name}: ${findEmployeeBySpecialization.name} - ${findEmployeeBySpecialization.department} - $${findEmployeeBySpecialization.salary}</p>`;
+    } else {
+        document.getElementById('employeesDetails').innerHTML = 'No employee has been found with this specialization';
     }
 }
